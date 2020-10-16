d858d0f34a2e2b5c186d16af8dfedfbfcdff8a2c
eskeppar <erik.skeppar@gmail.ocm>
2020-10-16 16:27:02 +0200

    Adds navbar and nicer styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { MaterialModule } from './material/material.module';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
-import { HttpClientModule } from '@angular/common/http';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { EduChooserComponent } from './components/edu-chooser/edu-chooser.component';
 import { OrderComponent } from './components/order/order.component';
 import { OrderFormComponent } from './components/order-form/order-form.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, AboutComponent, NavbarComponent, EduChooserComponent, OrderComponent, OrderFormComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    NavbarComponent,
+    EduChooserComponent,
+    OrderComponent,
+    OrderFormComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+
+const MaterialComponents = [
+  MatToolbarModule,
+  MatButtonModule,
+  MatIconModule,
+  MatCardModule,
+];
+
+@NgModule({
+  imports: [MaterialComponents],
+  exports: [MaterialComponents],
+})
+export class MaterialModule {}
